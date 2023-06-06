d176f578e03372714a7fb01f38157c3a4996a7df
tlglv-n <zander559@mail.ru>
2023-06-06 15:21:56 +0500

    +authViewModel

--- ExpenseTracker/ViewModel/AuthViewModel.swift
+++ ExpenseTracker/ViewModel/AuthViewModel.swift
@@ -9,7 +9,42 @@ import Foundation
 import Firebase
 
 class AuthViewModel {
-    private let authSerice: AuthServiceProtocol
+    private let authService: AuthServiceProtocol
     
+    init(authService: AuthServiceProtocol = AuthService()) {
+        self.authService = authService
+    }
+    
+    func login(email: String, password: String, completion: @escaping(Result<User, Error>) -> Void) {
+        authService.signIn(withEmail: email, password: password) { result in
+            switch result {
+            case .failure(let user):
+                completion(.failure(user))
+            case .success(let user):
+                completion(.success(user))
+            }
+        }
+    }
+    
+    func register(email: String, password: String, completion: @escaping(Result<User, Error>) -> Void) {
+        authService.createUser(withEmail: email, password: password) { result in
+            switch result {
+            case .success(let user):
+                let userModel = User(email: user.user.email!, password: password)
+                completion(.success(userModel))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+            
+        }
+    }
+    
+    func logout() {
+        do {
+            try authService.signOut()
+        } catch let error {
+            print("Error signing out: \(error)")
+        }
+    }
     
 }
