d4fb8b86dcb09bdec0259805ad3cc0e822c123c6
tlglv-n <zander559@mail.ru>
2023-06-10 13:44:43 +0500

    mainView

--- ExpenseTracker/View/MainViewController.swift
+++ ExpenseTracker/View/MainViewController.swift
@@ -9,21 +9,22 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var incomeLabel: UILabel!
+    @IBOutlet weak var outcomeLabel: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func addExpenseButtonPressed(_ sender: Any) {
     }
-    */
+    
+    @IBAction func settingsButtonPressed(_ sender: Any) {
+    }
+    
 
 }
